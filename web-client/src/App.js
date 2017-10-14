dcd2f950cc56e71707280df34be5465df141d45c
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-10-14 15:32:24 +0200

    play with routing

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 // COMPONENTS
 import Header from './components/Header';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
 //Stabs
-const Registration = () => <div>Registration</div>;
+const SomePage = () => <div>Some page that exists</div>;
+const NoMatch = () => <div>Page not exists</div>;
 
 class App extends Component {
     render() {
@@ -18,6 +19,10 @@ class App extends Component {
                     <Switch>
                         <Route exact path="/login" component={Login}/>
                         <Route exact path="/signup" component={Signup}/>
+
+                        <Route exact path="/somepage" component={SomePage}/>
+                        <Route exact path="/" render={() => (<Redirect to="/somepage" />)} />
+                        <Route path="*" component={NoMatch} />
                     </Switch>
                 </div>
             </BrowserRouter>
