309460d444059cca0aef80830179a3e5462a5a43
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-10-22 19:15:15 +0200

    mocked items from DB

---
+++ mobile/src/components/list/items.js
@@ -0,0 +1,48 @@
+export default items=[
+    {
+        _id: 'key1',
+        title: 'Item title',
+        description: ' very long yeah, long I told veryyyy'
+    },
+    {
+        _id: 'key2',
+        title: 'Item title',
+        description: 'this is long description , long I told veryyyy'
+    },
+    {
+        _id: 'key3',
+        title: 'Item title',
+        description: 'this  description very long yeah, long I told veryyyy'
+    },
+    {
+        _id: 'key4',
+        title: 'Item title',
+        description: 'this is  very long yeah, long I told veryyyy'
+    },
+    {
+        _id: 'key5',
+        title: 'Item title',
+        description: 'this veryyyy'
+    },
+    {
+        _id: 'key6',
+        title: 'Item title',
+        description: 'this is long description very lon'
+    },
+    {
+        _id: 'key7',
+        title: 'Item title',
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
+    },
+    {
+        _id: 'key8',
+        title: 'Item title',
+        description: 'this is long description very long yeah, long I told veryyyy'
+    },
+    {
+        _id: 'key9',
+        title: 'Item title',
+        description: 'this is long descrh, long I told veryyyy'
+    },
+];
+
