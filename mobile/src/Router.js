6a895ac1f647e7c6d66eebacfaf865264f930e1a
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-10-22 19:16:50 +0200

    update routing

--- mobile/src/Router.js
+++ mobile/src/Router.js
@@ -3,7 +3,10 @@ import { Stack, Scene, Router } from 'react-native-router-flux';
 import InitialScreen from "./components/InitialScreen";
 import RegistrationForm from "./components/RegistrationForm";
 import LoginForm from "./components/LoginForm";
-import List from "./components/list/List";
+import List from "./components/list/ListComponent";
+import { Actions } from 'react-native-router-flux';
+import CreateItem from './components/list/CreateItem';
+
 
 
 
@@ -17,7 +20,21 @@ const RouterComponent = () => {
                     <Scene key="loginForm" component={LoginForm} title="Login" backTitle="Home"/>
                 </Stack>
                 <Stack key="todo">
-                    <Scene key="list" component={List} title="ToDo list" initial/>
+                    <Scene
+                        key="list"
+                        component={List}
+                        title="ToDo list"
+                        onLeft={()=>console.log("logout pressed")}
+                        leftTitle="Logout"
+                        onRight={()=> Actions.createItem()}
+                        rightTitle="Add"
+                        initial
+                    />
+                    <Scene
+                        key="createItem"
+                        component={CreateItem}
+                        title="New Item"
+                    />
                 </Stack>
             </Stack>
         </Router>
