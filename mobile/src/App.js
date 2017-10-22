c690a203b1ec9bb9c6596a313d4930934756e848
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-10-22 10:32:03 +0200

    init mobile client

---
+++ mobile/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hallo
+        </Text>
+      </View>
+    );
+  }
+}
+export default App;
