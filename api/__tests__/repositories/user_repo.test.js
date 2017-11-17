d41d42bfeaa3cec0dc22016af0be669a2e1fdced
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-11-17 23:28:08 +0100

    update repo tests

--- api/__tests__/repositories/user_repo.test.js
+++ api/__tests__/repositories/user_repo.test.js
@@ -1,10 +1,14 @@
 process.env.NODE_ENV = 'test';
 // const server = require('../../server');
-const userRepository = require('../../repositories/user_repo');
 
-test('testing environment setup correct', () => {
-    expect(process.env.NODE_ENV).toEqual("test");
-});
+// const express = require('express');
+// const request = require('supertest');
+// const api = require('../../server');
+// const app = express();
+// const async = require('async');  // < ========= AMAZING LIBRARY https://github.com/caolan/async
+// app.use(api);
+
+const userRepository = require('../../repositories/user_repo');
 
 afterEach(() => {
     userRepository.cleanTable(function (cleaned) {
@@ -18,7 +22,15 @@ beforeEach(() => {
     });
 });
 
-test('registerUser()', (done)=>{
+
+
+it('Testing environment setup correct', (done) => {
+    expect(process.env.NODE_ENV).toEqual("test");
+    done();
+});
+
+
+test('Register new user', (done)=>{
     userRepository.registerUser('1@1.com', 'hashedPassword', function (err,user) {
         expect(user._id).not.toBeNull();
         expect(user.email).toEqual("1@1.com");
@@ -29,7 +41,7 @@ test('registerUser()', (done)=>{
 });
 
 
-test('getUserByEmail()', async (done)=>{
+test('Get user by email', async (done)=>{
      var callBack = () => {
          userRepository.getUserByEmail('1@1.com', (err, user) => {
              expect(user.password).toEqual("hashedPassword");
@@ -39,7 +51,7 @@ test('getUserByEmail()', async (done)=>{
      await userRepository.registerUser('1@1.com', 'hashedPassword', callBack);
 });
 
-test('getAll()', (done)=>{
+test('Get all users', (done)=>{
     var callBack = () => {
         userRepository.getAll({},(err,users)=>{
             // console.log(users)
