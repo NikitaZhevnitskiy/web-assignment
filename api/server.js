b18fecf948e63287fc5363d69d7b1e0403268d07
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-11-18 20:20:46 +0100

    add sockets api

--- api/server.js
+++ api/server.js
@@ -7,11 +7,31 @@ const mongoURI = require('./config/keys').mongoURI;
 
 app.use(bodyParser.json());
 app.use('/', routes);
+let httpServer;
 if(!module.parent) {
-    app.listen(port, () => console.log('Listening on port: ' + port + '\nNODE_ENV: ' + process.env.NODE_ENV + '\nmongoURI: ' + mongoURI));
+    httpServer = app.listen(port, () => console.log('Listening on port: ' + port + '\nNODE_ENV: ' + process.env.NODE_ENV + '\nmongoURI: ' + mongoURI));
 }
 
+
+// SOCKETS START
+const ws = require('ws');
+const wsServer = new ws.Server({
+    server: httpServer,
+});
+let sockets = [];
+wsServer.on('connection', (socket) => {
+    sockets.forEach(socket => socket.send("(server) New host connected"));
+    sockets.push(socket);
+    socket.send('(server) Hello, there!');
+
+    socket.on('close', () => {
+        console.log('disconnected');
+        sockets = sockets.filter(savedSocket => savedSocket !== socket);
+    });
+
+    socket.on('message', text => {
+        sockets.forEach(socket => socket.send(text));
+    });
+});
+// SOCKETS END
 module.exports = app;
-
-
-
