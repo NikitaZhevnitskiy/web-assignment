8177c71d082ea51730da85c9af8443b7eff12a44
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-11-19 13:36:29 +0100

    fix test (able to run suit tests with WebSockets)

--- api/server.js
+++ api/server.js
@@ -4,34 +4,41 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const mongoURI = require('./config/keys').mongoURI;
+const ws = require('ws');
 
 app.use(bodyParser.json());
 app.use('/', routes);
 let httpServer;
+
+let sockets = [];
 if(!module.parent) {
     httpServer = app.listen(port, () => console.log('Listening on port: ' + port + '\nNODE_ENV: ' + process.env.NODE_ENV + '\nmongoURI: ' + mongoURI));
+    chat(httpServer);
 }
 
 
 // SOCKETS START
-const ws = require('ws');
-const wsServer = new ws.Server({
-    server: httpServer,
-});
-let sockets = [];
-wsServer.on('connection', (socket) => {
-    sockets.forEach(socket => socket.send("(server) New host connected"));
-    sockets.push(socket);
-    socket.send('(server) Hello, there!');
-
-    socket.on('close', () => {
-        console.log('disconnected');
-        sockets = sockets.filter(savedSocket => savedSocket !== socket);
+function chat(http_server) {
+    const wsServer = new ws.Server({
+        server: http_server,
     });
 
-    socket.on('message', text => {
-        sockets.forEach(socket => socket.send(text));
+    wsServer.on('connection', (socket) => {
+        sockets.forEach(socket => socket.send("(server) New host connected"));
+        sockets.push(socket);
+        socket.send('(server) Hello, there!');
+
+        socket.on('close', () => {
+            console.log('disconnected');
+            sockets = sockets.filter(savedSocket => savedSocket !== socket);
+        });
+
+        socket.on('message', text => {
+            sockets.forEach(socket => socket.send(text));
+        });
     });
-});
+}
+
+
 // SOCKETS END
 module.exports = app;
